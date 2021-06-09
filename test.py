3c40bb28def2d6eecc1a6df1746a9eef25a39b4b
Rabindra <drarabin@gmail.com>
2021-06-09 22:37:07 +0545

    first commit

---
+++ test.py
@@ -0,0 +1,36 @@
+# a = 97
+# x = input()
+# for char in x:
+#     print(ord(char))
+
+# x = " "
+# print(x.isupper())
+# print(ord(x))
+
+# a = -62 % 26
+# print(a)
+# a += 97
+# print(a)
+# print(chr(a))
+
+# string = "hello"
+# # print(list(string))
+# string = list(string)
+# print(string)
+
+# key_keys = "abcdefghijklmnopqrstuvwxyz"
+# key_values = "qwertyuiopasdfghjklzxcvbnm"
+# key_keys = list(key_keys)
+# key_values = list(key_values)
+
+# # encryption_dict = {key_keys[i] : key_values[i] for i in range(len(key_keys))}
+# encryption_dict = dict (zip(key_keys, key_values))
+# print(encryption_dict.get('a'))
+
+# x = ' '
+# x = x.lower()
+# print(x, "asf")
+
+x = 'abcd'
+s = x.find('c')
+print(s)
