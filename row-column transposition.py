2559327de680dc7e5fed5ac652e6e65aac11573d
Rabindra <drarabin@gmail.com>
2021-07-05 08:00:15 +0545

    added comments to railfence and row transposition

--- row-column transposition.py
+++ row-column transposition.py
@@ -15,8 +15,10 @@ def encrypt():
     plaintext = plaintext.lower()
     plaintext = plaintext.replace(' ', '')
     plaintext = list(plaintext)
+    # add bogus letter 'z' to complete the matrix
     while(len(plaintext) % len(key) != 0):
         plaintext.append('z')
+    # convert list to nested list (array) with column size of len(key)
     plaintext = [plaintext[i:i+len(key)]
                  for i in range(0, len(plaintext), len(key))]
     ciphertext = list()
@@ -26,9 +28,10 @@ def encrypt():
         min_letter = min(temp_key)
         column = key.index(min_letter)
         temp_key.remove(min_letter)
+        # append the column with the index corresponding to the lowest value in the list key
         for i in range(len(plaintext)):
             ciphertext.append(plaintext[i][column])
-    print('Cipher text: ')
+    print('Cipher text:')
     print(''.join(ciphertext))
 
 
@@ -38,22 +41,24 @@ def decrypt():
     ciphertext = ciphertext.replace(' ', '')
     ciphertext = list(ciphertext)
 
-    column_length = len(ciphertext)//len(key)
+    column_length = len(ciphertext)//len(key)   # to calculate the no of columns i.e rowXcolumn = len(ciphertext)
     temp = list()
     temp_key = list()
+    # convert ilst to array
     temp = [ciphertext[i:i+column_length]
             for i in range(0, len(ciphertext), column_length)]
-    print(temp)
     plaintext = [[] for row in temp]
     temp_key.extend(key)
     i = 0
+    
     while(temp_key != []):
-        min_letter = min(temp_key)
-        row = key.index(min_letter)
-        temp_key.remove(min_letter)
-        plaintext[row] = temp[i]
+        min_letter = min(temp_key)    # calculate the minimum value in key
+        row = key.index(min_letter)   # get the index of the minimum value
+        temp_key.remove(min_letter)   # remove the min value from the temp_key for next loop
+        plaintext[row] = temp[i]      # assign the rows of temp/ciphertext matrix to its actual positions denoted by the row index
         i += 1
 
+    #transpose and convert to list/1D array
     plaintext = [row_matrix[i]
                  for i in range(column_length) for row_matrix in plaintext]
     print(''.join(plaintext))
